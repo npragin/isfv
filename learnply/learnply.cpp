e7ad54addf036938b19900aa1090fe33621e21fe
Brandon Bower <brandonkbower@gmail.com>
2024-11-21 13:56:35 -0800

    Changed learnply to show UI demo window

--- learnply/learnply.cpp
+++ learnply/learnply.cpp
@@ -5,6 +5,10 @@
 #include <fstream>
 #include <vector>
 
+#include "imgui.h"
+#include "imgui_impl_glut.h"
+#include "imgui_impl_opengl3.h"
+
 #include "glError.h"
 #include "GL/glew.h"
 #include "GL/freeglut.h"
@@ -80,6 +84,13 @@ void mousewheel(int wheel, int direction, int x, int y);
 void special(int key, int x, int y);
 void reshape(int width, int height);
 
+// Forward declarations of UI helper function
+void MainLoopStep();
+
+// Our UI state
+static bool show_demo_window = true;
+static bool show_another_window = false;
+static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
 /******************************************************************************
 Main program.
@@ -114,21 +125,37 @@ int main(int argc, char* argv[])
 
 	/*initialize openGL*/
 	init();
-	
-	/*the render function and callback registration*/
-	glutKeyboardFunc(keyboard);
-	glutDisplayFunc(display);
-	glutMotionFunc(motion);
-	glutMouseFunc(mouse);
-#ifdef _WIN32
-	glutMouseWheelFunc(mousewheel);
-#endif //_WIN32
-	glutSpecialFunc(special);
-	glutReshapeFunc(reshape);
 
-	/*event processing loop*/
-	glutMainLoop();
+	/*ui code start*/
+	glutDisplayFunc(MainLoopStep);
+
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+
+	ImGui::StyleColorsDark();
 	
+	ImGui_ImplGLUT_Init();
+	ImGui_ImplOpenGL3_Init();
+	ImGui_ImplGLUT_InstallFuncs();
+	/*ui code end*/
+
+	/*the render function and callback registration*/
+//	glutKeyboardFunc(keyboard);
+//	glutDisplayFunc(display);
+//	glutMotionFunc(motion);
+//	glutMouseFunc(mouse);
+//#ifdef _WIN32
+//	glutMouseWheelFunc(mousewheel);
+//#endif //_WIN32
+//	glutSpecialFunc(special);
+//	glutReshapeFunc(reshape);
+//
+//	/*event processing loop*/
+	glutMainLoop();
+//	
 	/*clear memory before exit*/
 	poly->finalize();	// finalize everything
 	if (ibfv)
@@ -138,10 +165,76 @@ int main(int argc, char* argv[])
 	}
 	delete(stopo);
 	delete(poly);
+	
+	/*cleanup ui*/
+	// Cleanup
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGLUT_Shutdown();
+	ImGui::DestroyContext();
 
 	return 0;
 }
 
+/******************************************************************************
+UI demo code
+******************************************************************************/
+
+void MainLoopStep()
+{
+	// Start the Dear ImGui frame
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplGLUT_NewFrame();
+	ImGui::NewFrame();
+	ImGuiIO& io = ImGui::GetIO();
+
+	// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+	if (show_demo_window)
+		ImGui::ShowDemoWindow(&show_demo_window);
+
+	// 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+	{
+		static float f = 0.0f;
+		static int counter = 0;
+
+		ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+		ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+		ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+		ImGui::Checkbox("Another Window", &show_another_window);
+
+		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+		ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+		if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+			counter++;
+		ImGui::SameLine();
+		ImGui::Text("counter = %d", counter);
+
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+		ImGui::End();
+	}
+
+	// 3. Show another simple window.
+	if (show_another_window)
+	{
+		ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+		ImGui::Text("Hello from another window!");
+		if (ImGui::Button("Close Me"))
+			show_another_window = false;
+		ImGui::End();
+	}
+
+	// Rendering
+	ImGui::Render();
+	glViewport(0, 0, (GLsizei)io.DisplaySize.x, (GLsizei)io.DisplaySize.y);
+	glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+	glClear(GL_COLOR_BUFFER_BIT);
+	//glUseProgram(0); // You may want this if using this code in an OpenGL 3+ context where shaders may be bound, but prefer using the GL3+ code.
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+	glutSwapBuffers();
+	glutPostRedisplay();
+}
 
 /******************************************************************************
 Init scene
