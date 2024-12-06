5a862d4eb78e47fd4947cb4f08e62987940d4c7b
npragin <npragin@gmail.com>
2024-12-06 14:49:08 -0800

    Create default colors for non-rainbow color maps

--- imgui/ui_window.h
+++ imgui/ui_window.h
@@ -10,6 +10,20 @@ struct color_choices {
 	float log_lab = 0.0;
 };
 
+struct DefaultColors {
+    std::vector<icVector3> singleHue = {icVector3(0.0, 0.0, 1.0)};
+    std::vector<icVector3> divergent = {
+        icVector3(0.1765, 0.0, 0.294),
+        icVector3(0.498, 0.231, 0.0314)
+    };
+    std::vector<icVector3> multiHue = {
+        icVector3(0.94, 0.98, 0.13),
+        icVector3(0.87, 0.11, 0.32),
+        icVector3(0.38, 0.0, 0.851),
+        icVector3(0.05, 0.03, 0.53)
+    };
+};
+
 void single_hue_window(color_choices* colors);
 
 void divergent_window(color_choices* colors);
